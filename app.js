c442fe2ac61635a8a51f19742fa53f7032904468
AkshataJagdev <jagdevakshata@gmail.com>
2025-03-01 13:19:46 -0500

    register login done

---
+++ app.js
@@ -0,0 +1,106 @@
+const express=require('express');
+const app=express();
+const {User}=require('./model/User');
+const mongoose = require('mongoose');
+const bcrypt=require('mongoose');
+const jwt=require('jsonwebtoken');
+const cors=require('cors');
+const morgan=require('morgan');
+
+
+//connecting to database
+mongoose.connect('mongodb://127.0.0.1:27017/shopify')
+.then(()=>{
+     console.log('Connected to database');
+}).catch((err)=>{
+    console.log('database is not Connected',err)
+})
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'))
+
+//task-1 ->create a register route
+app.post('/register',async(req,res)=>{
+  try{
+      
+    const {name,email,password}=req.body;
+    //check is an filed missing
+    if(!name || !email || !password){
+        return res.status(400).json({message:'some fields are missing'})
+    }
+
+    //check if user already exists
+    const isUserAlreadyExists=await User.findOne({email});
+    if(isUserAlreadyExists){
+        return res.status(400).json({message:'User already exists'})
+    }else{
+        //hashing the password
+        const salt=await bcrypt.genSaltSync(10);
+        const hashedpassword=await bcrypt.hashSync(password,salt);
+
+        //jwt token 
+        const token=jwt.sign({email},'supersecret',{expiresIn:'365d'});
+
+        //creaing new user
+        await User.create({
+            name,
+            email,
+            password:hashedpassword,
+            token,
+            role:'user'
+        })
+        return res.status(201).json({message:'User created successfully'})
+    }
+    
+  }catch(error){
+
+    console.log(error);
+    return res.status(500).json({message:'Internal server error'})
+  }
+})
+
+//task-2 -> create a login route
+app.post('/login',async(req,res)=>{
+    const {email,password}=req.body;
+          
+//check if any feild is missing 
+if(!email|| !password){
+    return res.status(400).json({message:'some fields are missing'});
+}
+//user exists or not
+const user=await User.findOne({email});
+if(!user){
+    return res.status(400).json({message:'user does not exists'});
+}
+//compare the enetred password with the hashed password
+const isPasswordMatched=await bcrypt.compareSync(password,user.password)
+if(!isPasswordMatched){
+    return res.status(400).json({message:"Passwprd is imcorrect"});
+}
+
+//succesfully logged in
+return res.status(200).json({
+    message:'user loggen in successfully',
+    id:user._id,
+    name:user.name,
+    email:user.email,
+    token:user.token,
+    role:user.role
+})
+
+
+    
+})
+
+
+
+
+
+
+
+const PORT=8080;
+app.listen(PORT,()=>{
+    console.log(`server is connected to port ${PORT}`)
+})
